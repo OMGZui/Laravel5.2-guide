29757e9a88ea5adcb198bd4cbb51c31f0fef4dfc
zui <862275679@qq.com>
2016-08-19 11:19:52 +0800

    Laravel5.2入门blog

---
+++ config/web.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'web_title' => 'OMGZui Blog系统',
+  'web_count' => '百度统计',
+  'web_status' => '1',
+  'seo_title' => 'blog',
+  'keywords' => 'Laravel',
+  'description' => '利用Laravel5.2开发的一套blog系统',
+  'copyright' => 'Design by OMGZui <a href="https://github.com/OMGZui" target="_blank">OMGZui</a>',
+);
