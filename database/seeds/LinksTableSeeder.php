fca7edfe104c7201a742deff8b96e8877f9a3108
zui <862275679@qq.com>
2016-08-19 11:51:24 +0800

    links

--- database/seeds/LinksTableSeeder.php
+++ database/seeds/LinksTableSeeder.php
@@ -13,17 +13,11 @@ class LinksTableSeeder extends Seeder
     {
         $data = [
             [
-                'link_name' => '后盾网',
-                'link_title' => '国内口碑最好的PHP培训机构',
-                'link_url' => 'http://www.houdunwang.com',
+                'link_name' => 'OMGZui',
+                'link_title' => 'blog',
+                'link_url' => 'http://www.github.com/OMGZui',
                 'link_order' => 1,
             ],
-            [
-                'link_name' => '后盾论坛',
-                'link_title' => '后盾网，人人做后盾',
-                'link_url' => 'http://bbs.houdunwang.com',
-                'link_order' => 2,
-            ]
         ];
         DB::table('links')->insert($data);
     }
